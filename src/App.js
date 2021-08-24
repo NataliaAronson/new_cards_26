abf8f483f584dfd02cec70698139d5ea295744c7
Natalia Aronson <nataronso@gmail.com>
2021-08-24 19:31:50 +0200

    adding pictures

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Ucheniy from './components/Catpictures/Ucheniy.jpg';
 import Puss from './components/Catpictures/Puss.jpg';
 import Leopold from './components/Catpictures/Leopold.jpg';
 import  Tom from './components/Catpictures/Tom.jpg';
-import  Cheshire from './components/Catpictures/ Cheshire.jpg';
+import  Cheshire from './components/Catpictures/Cheshire.jpg';
 
 class CardList extends React.Component {
   render() {
@@ -15,37 +15,37 @@ class CardList extends React.Component {
         <React.Fragment>
 
         <Card
-        src={Matroskin} alt{Matroskin}
+        src={Matroskin} 
         name="Matroskin "
         description="A can who can talk , business savvy and responsible for Dyada Fedor and Sharik"
         location="Prostokvashino"
          />
          <Card
-        src={Ucheniy} alt{Ucheniy}
+        src={Ucheniy} 
         name="Ucheniy"
         description="A cat who is the best in IT and able to do magic"
         location=" Itgirlschool"
          />
          <Card
-        src={Puss}alt{Puss}
+        src={Puss}
         name="Puss "
         description=" Shek's  best friend "
         location="Shek's Neverland"
          />
          <Card
-        src={Leopold}alt{Leopold}
+        src={Leopold}
         name="Leopold"
         description=" Podliy Trus. Not really.Believes friendship rules. A peacemaker."
         location="Souzmultfilm"
          />
          <Card
-        src={Tom}alt{Tom}
+        src={Tom}
         name=" Tom"
         description="A can who gets in trouble almost all the time because of his neighbour Jerry"
         location=" Tom and Jerry cartoon"
          />
          <Card
-        src={Cheshire}alt{Cheshire}
+        src={Cheshire}
         name="Cheshire"
         description="A can who knows life philosophy and he is Alice's best friend"
         location=" Alice in Wodrerland"
